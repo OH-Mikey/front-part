b41610646114ac03611654f45e900aa32afc32dd
flyc_chung <flyc_chung@paradise-soft.com.tw>
2017-11-15 17:27:54 +0800

    basic login layout and accordion works, but it spent me so lots of time...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,53 @@
 document.addEventListener("DOMContentLoaded", function() {
-	console.log('javascript load');
+
+	new Vue({
+		el: "#app",
+		data: {
+			account: '',
+			accountError: '',
+			password: '',
+			passwordError: '',
+			passwordConfirm: '',
+			passwordConfirmError: ''
+		},
+		methods: {
+			registerSubmit: function() {
+				console.log("something");
+			}
+		},
+		watch: {
+			account: function(input) {
+				console.log();
+			},
+			password: function(input) {
+				this.passwordError = '';
+				this.passwordConfirmError = '';
+				if (input.length < 8) {
+					this.passwordError = 'password should at least 8 words!';
+				}
+				if (this.password !== input) {
+					this.passwordConfirmError = 'password confirm not match!';
+				}
+			},
+			passwordConfirm: function(input) {
+				if (this.password !== input) {
+					this.passwordConfirmError = 'password confirm not match!';
+				} else {
+					this.passwordConfirmError = '';
+				}
+			}
+		},
+		mounted: function() {
+			$('.accordion').accordion({
+				onOpening: function() {
+					$('.ui.accordion .title').css({
+						opacity: 0,
+						pointerEvents: 'none'
+					});
+				}
+			});
+		}
+	});
+
+
 });
