3b51272c889f7fdb1226588a03bc922736ab330f
flyc_chung <flyc_chung@paradise-soft.com.tw>
2017-11-16 16:29:45 +0800

    include d3 success

--- assets/js/analysis.js
+++ assets/js/analysis.js
@@ -1,9 +1,29 @@
 new Vue({
-    el:"#app",
-    data:{
+    el: "#app",
+    data: {
         loginStatus: true
     },
-    components:{
+    components: {
         componentHeader: componentHeader
+    },
+    mounted: function() {
+        d3.select('.analysis_content')
+            .append('svg')
+            .attr({
+                width: '100%',
+                height: '100%',
+            })
+            .style({
+                border: '3px solid blue'
+            })
+            .append('circle')
+            .attr({
+                'cx': "50%",
+                'cy': "50%",
+                'r': 30,
+                'fill': '#f90',
+                'stroke': '#c00',
+                'stroke-width': '5px'
+            });
     }
 });
