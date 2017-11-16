d2c16cde7e2df1f0c9d34d617f6abb262bc08520
flyc_chung <flyc_chung@paradise-soft.com.tw>
2017-11-16 17:50:50 +0800

    create primise function handle axios

--- assets/js/analysis.js
+++ assets/js/analysis.js
@@ -7,23 +7,25 @@ new Vue({
         componentHeader: componentHeader
     },
     mounted: function() {
-        d3.select('.analysis_content')
-            .append('svg')
-            .attr({
-                width: '100%',
-                height: '100%',
-            })
-            .style({
-                border: '3px solid blue'
-            })
-            .append('circle')
-            .attr({
-                'cx': "50%",
-                'cy': "50%",
-                'r': 30,
-                'fill': '#f90',
-                'stroke': '#c00',
-                'stroke-width': '5px'
-            });
+        getChart().then(function(output) {
+            console.log(output);
+        }).catch(function(output) {
+            console.log(output);
+        });
     }
 });
+
+
+function getChart(input) {
+    return new Promise(function(resolve, reject) {
+        axios({
+            method: 'get',
+            // url: '/',
+            url: 'http://localhost:3000/chart'
+        }).then(function(res) {
+            resolve(res.data);
+        }).catch(function(error) {
+            reject(error);
+        });
+    });
+}
