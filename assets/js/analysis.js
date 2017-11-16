ff8fe9672dace9360eb0aab0208ceecdfeece450
flyc_chung <flyc_chung@paradise-soft.com.tw>
2017-11-16 18:53:30 +0800

    add offset animation and random data of chart works

--- assets/js/analysis.js
+++ assets/js/analysis.js
@@ -7,8 +7,92 @@ new Vue({
         componentHeader: componentHeader
     },
     mounted: function() {
-        getChart().then(function(output) {
-            console.log(output);
+        getChart().then(function(data) {
+
+            var width = window.innerWidth * 0.8,
+                height = 683;
+
+            var minX = d3.min(data, function(d) {
+                return d.x;
+            });
+            var maxX = d3.max(data, function(d) {
+                return d.x;
+            });
+            var minY = d3.min(data, function(d) {
+                return d.y;
+            });
+            var maxY = d3.max(data, function(d) {
+                return d.y;
+            });
+
+            var scaleX = d3.scale.linear()
+                .range([0, width])
+                .domain([minX, maxX]); //x 的最大值與最小值
+
+            var scaleY = d3.scale.linear()
+                .range([height, 0]) //反過來才會上下顛倒
+                .domain([minY, maxY]); //y 的最大值與最小值
+
+            var axisX = d3.svg.axis()
+                .scale(scaleX)
+                .orient("bottom")
+                .ticks(20);
+
+            var axisY = d3.svg.axis()
+                .scale(scaleY)
+                .orient("left")
+                .ticks(10);
+
+
+            var svg = d3.select('.analysis_content')
+                .append('svg')
+                .attr({
+                    'width': '100%',
+                    'height': '100%'
+                });
+
+            var line = d3.svg.line()
+                .x(function(d) {
+                    return scaleX(d.x);
+                })
+                .y(function(d) {
+                    return scaleY(d.y);
+                })
+                .interpolate('step');
+            svg.append('path')
+                .attr({
+                    'id': 'offsetId',
+                    'd': line(data),
+                    'y': 0,
+                    'stroke': '#000',
+                    'stroke-width': '1px',
+                    'fill': 'none',
+                    'transform': 'translate(35,20)' //折線圖也要套用 translate
+
+                });
+
+            svg.append('g')
+                .call(axisX)
+                .attr({
+                    'fill': 'none',
+                    'stroke': '#000',
+                    'transform': 'translate(35,' + (height + 20) + ')'
+                });
+
+            svg.append('g')
+                .call(axisY)
+                .attr({
+                    'fill': 'none',
+                    'stroke': '#000',
+                    'transform': 'translate(35,20)'
+                });
+
+            var path = document.querySelector('#offsetId');
+            var length = path.getTotalLength();
+            path.style['stroke-dasharray'] = Math.ceil(length);
+            path.style['stroke-dashoffset'] = Math.ceil(length);
+            path.classList.add('run');
+
         }).catch(function(output) {
             console.log(output);
         });
@@ -29,3 +113,16 @@ function getChart(input) {
         });
     });
 }
+
+var array = [];
+for (var i = 0; i < 25; i++) {
+    array.push({
+        x: i,
+        y: Math.round(Math.random() * 100)
+    });
+    array.push({
+        x: i+1,
+        y:0
+    });
+}
+console.log(array);
